e51fdaa7eaf044b1293489bee66a4a96688ccb6f
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-09-11 22:13:27 +0300

    Generics [Swap Ex]

---
+++ Demo/Helper.cs
@@ -0,0 +1,51 @@
+﻿using Demo.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Helper<T>
+    {
+        // In case generic type <T> is declared on method level not class Or Struct Or Interface
+        // Compiler can detect the type of T based of Passed Parameters
+        
+        #region non Generic
+        //public static void Swap(ref int a, ref int b)
+        //{
+        //    int temp = a;
+        //    a = b;
+        //    b = temp;
+        //}
+
+        //public static void Swap(ref double a, ref double b)
+        //{
+        //    double temp = a;
+        //    a = b;
+        //    b = temp;
+        //}
+
+        //public static void Swap(ref Point a, ref Point b)
+        //{
+        //    Point temp = a;
+        //    a = b;
+        //    b = temp;
+        //} 
+        #endregion
+
+        #region Generics
+
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+
+        #endregion
+
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Demo
+﻿using Demo.Generics;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Generics [Swap Ex]
+
+            #region Non Generic way
+            //int a = 10, b = 20;
+            //Console.WriteLine($"Before Swap: a={a}, b={b}");
+            //Helper.Swap(ref a, ref b);
+            //Console.WriteLine($"After Swap: a={a}, b={b}");
+
+            //double a = 10.5, b = 20.5;
+            //Console.WriteLine($"Before Swap: a={a}, b={b}");
+            //Helper.Swap(ref a, ref b);
+            //Console.WriteLine($"After Swap: a={a}, b={b}");
+
+            //Point p1 = new Point(10, 20);
+            //Point p2 = new Point(30, 40);
+            //Console.WriteLine($"Before Swap: p1={p1}, p2={p2}");
+            //Helper.Swap(ref p1, ref p2);
+            //Console.WriteLine($"After Swap: p1={p1}, p2={p2}");
+            #endregion
+
+            #region Generic Way
+            int a1 = 10, b1 = 20;
+            Console.WriteLine($"Before Swap: a={a1}, b={b1}");
+            Helper<int>.Swap(ref a1, ref b1);
+            Console.WriteLine($"After Swap: a={a1}, b={b1}");
+
+            double a = 10.5, b = 20.5;
+            Console.WriteLine($"Before Swap: a={a}, b={b}");
+            Helper<double>.Swap(ref a, ref b);
+            Console.WriteLine($"After Swap: a={a}, b={b}");
+
+            Point p1 = new Point(10, 20);
+            Point p2 = new Point(30, 40);
+            Console.WriteLine($"Before Swap: p1={p1}, p2={p2}");
+            Helper<Point>.Swap(ref p1, ref p2);
+            Console.WriteLine($"After Swap: p1={p1}, p2={p2}");
+
+            #endregion
+
+            #endregion
         }
     }
 }
